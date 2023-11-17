ceb026851ee31a2b27980764bae35472f2e7000c
mariagomezv <mariaj.gomezv@upb.edu.co>
2023-11-16 20:05:07 -0500

    estetica placentera

--- logic/api/main.py
+++ logic/api/main.py
@@ -1,10 +1,8 @@
 from fastapi import FastAPI, HTTPException, Request
-from fastapi.templating import Jinja2Templates
 import httpx
 import mysql.connector
 
 app = FastAPI()
-templates = Jinja2Templates(directory="templates")
 
 # Configuración de la base de datos
 db_config = {
